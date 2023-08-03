8e793426ab2ede8dc37d82e4ef6735a2e4e6ab0e
Ricardo Carrasquero <ricardo.carrasquero@chilecompra.cl>
2023-08-03 18:57:48 -0400

    feat: add dark mode

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,8 +1,37 @@
+import { useEffect, useState } from 'react'
+
 /**
  * The home page
  */
 const Home = () => {
-  return <div className="flex h-screen items-center justify-center text-xl font-bold">Home</div>
+  const [isDarkMode, setIsDarkMode] = useState(() => {
+    const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches
+    const theme = localStorage.getItem('theme')
+
+    return theme ? theme === 'dark' : prefersDarkMode
+  })
+
+  useEffect(() => {
+    if (isDarkMode) {
+      document.documentElement.classList.add('dark')
+    } else document.documentElement.classList.remove('dark')
+  }, [isDarkMode])
+
+  const handleToggleTheme = () => {
+    setIsDarkMode(!isDarkMode)
+    localStorage.setItem('theme', !isDarkMode ? 'dark' : 'light')
+  }
+
+  return (
+    <div className="flex h-screen items-center justify-center bg-white text-lg font-bold text-white dark:bg-gray-400">
+      <button
+        className="rounded-3xl bg-gray-400 p-4 dark:bg-white dark:text-gray-400"
+        onClick={handleToggleTheme}
+      >
+        Light / Dark
+      </button>
+    </div>
+  )
 }
 
 export default Home
